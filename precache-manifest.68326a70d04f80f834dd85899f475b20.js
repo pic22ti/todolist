76c3041e295990424d74b2d7d72ea11d76d5c1ed
pic22ti <65947265+pic22ti@users.noreply.github.com>
2020-10-15 10:59:13 +0900

    Updates

---
+++ precache-manifest.68326a70d04f80f834dd85899f475b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db715bb1783066d0cd0f0024ab458f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "42fb9d9690cf3e941d8a",
+    "url": "/todolist/static/css/main.84ab3108.chunk.css"
+  },
+  {
+    "revision": "d2c03822584bc91fd46b",
+    "url": "/todolist/static/js/2.b8c37fee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist/static/js/2.b8c37fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fb9d9690cf3e941d8a",
+    "url": "/todolist/static/js/main.2b04987a.chunk.js"
+  },
+  {
+    "revision": "be1ea64cf07e1e2b9473",
+    "url": "/todolist/static/js/runtime-main.992438bd.js"
+  }
+]);
